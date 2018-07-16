2434d4bcb08f994175bf0cee4d1ccdd13ed172e7
Marissa Zhong <mwz2101@columbia.edu>
2018-07-16 14:45:25 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 2) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
     t.string "password"
+    t.string "username"
+    t.decimal "balance"
   end
 
 end
